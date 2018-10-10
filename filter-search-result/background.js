b838cfed58d76e28502dc98d8be60c4651d8ad10
CimXin <372134690@qq.com>
2018-10-10 20:59:42 +0800

    init

---
+++ filter-search-result/background.js
@@ -0,0 +1,23 @@
+/** 安装完成回调 */
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({
+        color: '#3aa757'
+    }, function () {
+        console.log('The color is green.');
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        test(1);
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    hostEquals: 'developer.chrome.com'
+                },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+});
+
+function test(str) {
+    console.log("add log: ", str);
+}
